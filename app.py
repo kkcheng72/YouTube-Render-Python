2c9cf27e6bb7b2c85f7e89c94804e12bf6bad425
kkcheng <kaikei@hotmail.com>
2023-10-02 15:05:29 +0800

    app.get

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello Hello'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello Hello'
+
+@app.get('/')
+def index():
+    return{'message': 'Hello, World!'}
 
 if __name__ == '__main__':
     app.run()
